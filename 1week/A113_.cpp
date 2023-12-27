11581f99d7c6b69878ea72bd62664aa8592130ec
minzziPark <minzzi0068@gmail.com>
2023-12-27 13:11:35 +0900

    Create A113_.cpp

---
+++ 1week/A113_.cpp
@@ -0,0 +1,19 @@
+/*
+연결 요소의 개수
+Source: https://www.acmicpc.net/problem/11724
+2023/01/01
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int N, M;
+	cin >> N >> M;
+
+	for(int i=0; i<M; i++){
+		
+	}
+
+	return 0;
+}
