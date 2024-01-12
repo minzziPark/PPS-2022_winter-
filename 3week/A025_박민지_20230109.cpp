6b591a8cf3fc790fabd3607b92b848d3d786f47a
minzziPark <minzzi0068@gmail.com>
2024-01-12 18:48:42 +0900

    Create A025_박민지_20230109.cpp

---
+++ 3week/A025_박민지_20230109.cpp
@@ -0,0 +1,39 @@
+/*
+Source: https://leetcode.com/problems/power-of-four/
+
+342. Power of Four
+
+Given an integer n, return true if it is a power of four. 
+Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+Example 1:
+	Input: n = 16
+	Output: true
+
+Example 2:
+	Input: n = 5
+	Output: false
+
+Example 3:
+	Input: n = 1
+	Output: true
+*/
+#include <cmath>
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        bool result = true;
+
+		while(n != 0){
+			if(n == 1) return result;
+			if(n%4 != 0) result = false;
+			n /= 4;
+		}
+
+		if(n == 0) result = false;
+
+		return result;
+    }
+};
