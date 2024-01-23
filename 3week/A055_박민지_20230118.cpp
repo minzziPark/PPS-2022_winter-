cb80e006017fcf8cf7fcd7446cc97cf6efc3f594
minzziPark <minzzi0068@gmail.com>
2024-01-23 15:45:06 +0900

    Create A055_박민지_20230118.cpp

---
+++ 3week/A055_박민지_20230118.cpp
@@ -0,0 +1,64 @@
+/*
+Source: https://leetcode.com/problems/remove-outermost-parentheses/
+
+1021. Remove Outermost Parentheses
+
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+Example 1:
+
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: s = "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+*/
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+		string result = "";
+		string cut = "";
+		int s_idx = 0;
+		int e_idx = 0;
+		int cnt_left = 0;
+        for(int i=0; i<s.size(); i++){
+			if(cut.size() == 0){
+				s_idx = i;
+			}
+			if(s[i] == '('){
+				cnt_left++;
+			}else if(s[i] == ')'){
+				cnt_left--;
+				if(cnt_left == 0){
+					e_idx = i+1;
+					cut = s.substr(s_idx, e_idx-s_idx);
+					result += cut;
+					cnt = "";
+				}
+			}
+		}
+		return result;
+    }
+};
+
