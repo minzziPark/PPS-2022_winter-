8fd4f686618d2bfa304a82c068379cf4cff3376f
minzziPark <minzzi0068@gmail.com>
2024-01-27 03:48:35 +0900

    Create A058_박민지_20230119.cpp

---
+++ 3week/A058_박민지_20230119.cpp
@@ -0,0 +1,53 @@
+/*
+Source: https://leetcode.com/problems/reverse-linked-list/
+
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+Input: head = []
+Output: []
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+		ListNode* node = nullptr;
+
+		while(head != nullptr){
+            ListNode* result = new ListNode;
+            if(node == nullptr){
+                node = new ListNode;
+                result->val = head->val;
+                result->next = nullptr;
+                node = result;
+            }
+			else{
+                result->val = head->val;
+                result->next = node;
+                node = result;
+            }
+            head = head->next;
+		}
+       
+		return node;
+    }
+};
